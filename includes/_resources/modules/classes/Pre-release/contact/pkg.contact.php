2cfe603311d2caa5c68253a2688346b8676d1f8a
Martin <martin@writells.com>
2015-07-26 21:32:46 +0100

    Contact module added
     * Added an advanced contact module

--- includes/_resources/modules/classes/Pre-release/contact/pkg.contact.php
+++ includes/_resources/modules/classes/Pre-release/contact/pkg.contact.php
@@ -34,6 +34,22 @@ class contact {
             $html = htmlentities('<div class="row rowContacts" id = "bindPlanner">');
             $html = htmlentities('<div class="col-md-8">');
             $html = htmlentities('<div class="col-md-8">');
+            $html = htmlentities('<div class="addContactList">');
+            $html = htmlentities('<script type="text/ng-template" id="customTemplate.html">');
+            $html = htmlentities('<a>');
+            $html = htmlentities('<img ng-src="http://upload.wikimedia.org/wikipedia/commons/thumb/{{match.model.flag}}" width="16">');
+            $html = htmlentities('<span bind-html-unsafe="match.label | typeaheadHighlight:query"></span>');
+            $html = htmlentities('</a>');
+            $html = htmlentities('</script>');
+            $html = htmlentities('<div class="container-fluid">');
+            $html = htmlentities('<h1>Add New Contact</h1><br>');
+            $name = htmlentities('<input type="text" ng-model="FirstName" placeholder="First Name" typeahead=" Fname for Fname in names | filter:$viewValue | limitTo:8" class="form-control">');
+            $lname= htmlentities('<input type="text" ng-model="LastName"  placeholder="Last Name" typeahead="  Lname for Lname in names | filter:$viewValue | limitTo:8" class="form-control">');
+            $dob =  htmlentities('<input type="datetime" ng-model="D.O.B" placeholder="D.O.B" class="form-control">');
+            $email =htmlentities('<input type="email" ng-model="Email" placeholder="@ Address" class="form-control">');
+            $phone =htmlentities('<input type="number" ng-model="Tel" placeholder="Telephone Number" class="form-control">');
+            $location =htmlentities('<input type="number" ng-model="Tel" placeholder="Telephone Number" class="form-control">');
+            $note =htmlentities('<input type="text" ng-model="Note" placeholder="Note" class="form-control">');
         }elseif($option === 'edit'){
 
         }else{
