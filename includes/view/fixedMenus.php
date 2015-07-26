d3a597bade7dc1aeaf81c7363f236b08742ec1c2
Martin <martin@writells.com>
2015-07-26 11:39:43 +0100

    Rightbar Menu
      * Added a right bar menu for quick access
      * Updated 404 error logging

---
+++ includes/view/fixedMenus.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martin
+ * Date: 19/07/2015
+ * Time: 01:04
+ */
+?>
+
+<div class="quickMenu">
+    <ul>
+        <li>
+            <img src="/img/icons/partnereink.png">
+            <ul><li>Contacts</li></ul>
+        </li>
+        <li>
+            <img src="/img/icons/contacten-icon.png">
+            <ul><li>Groups</li></ul>
+        </li>
+        <li>
+            <img src="/img/icons/messages.png">
+            <ul><li>Messages</li></ul>
+        </li>
+        <li>
+            <img src="/img/icons/time.png">
+            <ul><li>Events</li></ul>
+        </li>
+        <li>
+            <img src="/img/icons/serviceIcon.png">
+            <ul><li>Planner</li></ul>
+        </li>
+    </ul>
+</div>
