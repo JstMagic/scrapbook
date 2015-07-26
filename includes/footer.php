578cdca0e66983b8b2b0cbb2b11685a3ef3a693a
Martin <martin@writells.com>
2015-07-26 11:37:04 +0100

    Rightbar Menu
      * Added a right bar menu for quick access
      * Updated 404 error logging

--- includes/footer.php
+++ includes/footer.php
@@ -9,15 +9,22 @@
 <script type="text/javascript" src="http://scrapbook.writells.com/js/features.js"></script>
 <script>
     $.backstretch([
-        "http://www.paulbondart.com/paintings/Shelter.jpg"
+        /*"http://www.paulbondart.com/paintings/Shelter.jpg"
         , "http://webneel.com/wallpaper/sites/default/files/images/08-2013/8-star-fish-sea-shell-beach-wallpaper.jpg"
         , "http://xdesktopwallpapers.com/wp-content/uploads/2010/12/Rainy-Day-In-Morning.jpg"
         , "http://www.alphawallpapers.com/wp-content/uploads//2009/03/arch_zuzuka_3.jpg"
-//        , "http://dl.dropbox.com/u/515046/www/outside.jpg"
+        , "http://dl.dropbox.com/u/515046/www/outside.jpg"
         , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
         , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
         , "http://www.crossingitaly.net/travel/wp-content/uploads/Beach-holidays.jpg"
-        , "http://www.mamargaretshouse.com/blog/wp-content/uploads/2015/01/IMG_0072.jpg"
+        , "http://www.mamargaretshouse.com/blog/wp-content/uploads/2015/01/IMG_0072.jpg"*/
+         "http://www.wallpaperup.com/uploads/wallpapers/2012/08/04/8488/69e1c1b783451063140d11210d8f7bff.jpg",
+         "http://wallpoper.com/images/00/36/81/90/gradient_00368190.jpg",
+         "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQa239_IJiycGtwv8V1QlodXvG-ADsbZyP0u60LzYb6WV2lPgfs",
+         "http://www.twitterevolutions.com/bgs/twitter-background-grungy-green-wall.jpg",
+         "http://www.hdwallpaperscool.com/wp-content/uploads/2013/11/neautiful-water-drop-top-images-computer-background-hd-wallpapers1.png",
+         "http://www.hdwallpapersinn.com/wp-content/uploads/2014/07/splash-of-color_1920x1080-HD-Wallpapers.jpg",
+         "http://p1.pichost.me/i/73/1980498.jpg"
     ], {duration: 3000, fade: 750});
 </script>
 
